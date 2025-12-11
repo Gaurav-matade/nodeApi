7576021eb1f36ae04dc733b6947d9d82b5c3674d
Gaurav-matade <gauravmatade2610@gmail.com>
2025-12-11 11:03:10 +0530

    fixd live db connection code

--- src/DB/connection.ts
+++ src/DB/connection.ts
@@ -1,16 +1,24 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
 
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
-  process.env.DB_USER as string,
-  process.env.DB_PASSWORD as string,
+  process.env.DB_NAME || '',
+  process.env.DB_USER || '',
+  process.env.DB_PASSWORD || '',
   {
     host: process.env.DB_HOST,
-    dialect: "mysql",
+    port: Number(process.env.DB_PORT) || 3306,
+    dialect: 'mysql',
     logging: false,
+    dialectOptions: {
+      ssl: {
+        ca: fs.readFileSync(path.join(__dirname, '../../certificate/ca.pem')),
+      },
+    },
   }
 );
 
